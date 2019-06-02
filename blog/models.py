50125a0a9b9efd2733a46f1fd57a3d0fc8c37fd8
Your Name <you@example.com>
2019-06-02 23:07:47 +1000

    images migration

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    img = models.TextField(default="")
+    image = models.TextField(default="")
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
